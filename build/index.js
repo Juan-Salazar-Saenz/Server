8afc5997e46ed17b76ae6bc2f1ecbdafc0ca3453
Juan Salazar Saenz <tec.juan.0927cacd@gmail.com>
2020-04-13 01:30:04 -0500

    Segund

--- build/index.js
+++ build/index.js
@@ -39,7 +39,7 @@ class Server {
     }
     start() {
         this.app.listen(this.app.get('port'), () => {
-            console.log('Server on port', this.app.get('port'), "192.168.0.24");
+            console.log('Server on port', this.app.get('port'));
         });
     }
 }
